1818b998b9a0780268da94c0762c73abae04cb71
Lorenzo García Moreno <dev@lorenzogm.com>
2022-04-29 16:58:53 +0200

    fix: lint

--- packages/ui-tailwind-preset/src/createPreset.ts
+++ packages/ui-tailwind-preset/src/createPreset.ts
@@ -1,5 +1,6 @@
 import { getDesignTokens } from '@ring/ui-design-tokens';
 
 export function createPreset() {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return getDesignTokens();
 }
