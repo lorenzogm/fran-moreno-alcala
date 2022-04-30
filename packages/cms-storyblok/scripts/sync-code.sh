0d41cdf5e44bb0e79ed2d0f2bcd38de448241a5b
Lorenzo García Moreno <dev@lorenzogm.com>
2022-04-30 18:02:24 +0200

    build: run prettier after sync storyblok

--- packages/cms-storyblok/scripts/sync-code.sh
+++ packages/cms-storyblok/scripts/sync-code.sh
@@ -11,5 +11,8 @@ rm ./components.$STORYBLOK_SOURCE_SPACE.json
 cp ./presets.$STORYBLOK_SOURCE_SPACE.json ./config/
 rm ./presets.$STORYBLOK_SOURCE_SPACE.json
 
-# push
-storyblok-generate-ts source=./config/components.$STORYBLOK_SPACE_ID.json target=./src/types/generated/storyblok.types.ts
+# generate-ts
+storyblok-generate-ts source=./config/components.$STORYBLOK_SOURCE_SPACE.json target=./src/types/generated/storyblok.types.ts
+
+cd ../..
+yarn format
